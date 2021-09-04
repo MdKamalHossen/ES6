c8c2d4dfceeaa6b7dcbda26e973cc0d25dfdab6d
Kamal Hossen <kamalbubt63@gmail.com>
2021-09-04 21:17:30 +0600

    new

---
+++ defult.js
@@ -0,0 +1,5 @@
+function add(number1, number2 = 10){
+     return number1 + number2;
+}
+const total = add(100);
+console.log(total);
---
+++ letconst.js
@@ -0,0 +1,7 @@
+
+let sum = 0;
+let number = 10
+for(let i= 0 ; i<10; i++){
+     sum = sum + i;
+}
+console.log(i); //this is a scop variable
